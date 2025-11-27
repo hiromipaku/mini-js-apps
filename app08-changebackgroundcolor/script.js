cb82b2af244bec63713642c4984f1861d0b54a57
Hiromi Yamamoto <hiromi.ymmt.0608@gmail.com>
2025-11-27 15:18:59 +1300

    changebackground-color app

---
+++ app08-changebackgroundcolor/script.js
@@ -0,0 +1,9 @@
+const btn = document.querySelector('#btn');
+
+btn.addEventListener('click', () => {
+  const r = Math.floor(Math.random() * 256);
+  const g = Math.floor(Math.random() * 256);
+  const b = Math.floor(Math.random() * 256);
+
+  document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+});
